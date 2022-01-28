09a5f5f60096aae08152a0093dbb4094b73ed71f
Alex Hubers <ahubers15@cornellcollege.edu>
2022-01-28 15:51:32 -0600

    Tweak higher kinded example

--- testsuite/tests/party-ctrs-ts/HigherKinded.hs
+++ testsuite/tests/party-ctrs-ts/HigherKinded.hs
@@ -8,5 +8,9 @@ data (Eq a, Eq b) => OR a b = L a | R b
 
 -- Honestly don't know if this is a problem or not.
 -- Currently my WF_F kind signature will be wrong.
-type family F (a :: *) :: * -> * where
-  F a = OR a
+type family F (a :: *) :: * -> *
+type instance F a = OR a
+
+foobar :: F a b -> Bool
+foobar (L a) = a == a
+foobar (R b) = b == b
