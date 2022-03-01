60eddfa459fd1a5483ccead27c3a21d21013443e
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2022-03-01 10:52:25 -0600

    expose IndexCO to the world

--- compiler/GHC/Cmm/Ppr.hs
+++ compiler/GHC/Cmm/Ppr.hs
@@ -129,6 +129,7 @@ pprTopInfo platform (TopInfo {info_tbls=info_tbl, stack_info=stack_info}) =
 pprBlock :: (
 #if MIN_VERSION_base(4,16,0)
   WFT (IndexedCO x SDoc SDoc),
+  WFT (IndexedCO e SDoc SDoc),
 #endif
   IndexedCO x SDoc SDoc ~ SDoc)
          => Platform -> Block CmmNode e x -> IndexedCO e SDoc SDoc
