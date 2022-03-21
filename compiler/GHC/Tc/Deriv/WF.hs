04c58d72d9fb02e627b8830229398e000e55920c
Alex Hubers <ahubers15@cornellcollege.edu>
2022-03-21 10:25:21 -0500

    Revert "Remove unused funs genWFMirrorTyCon(s)"
    This reverts commit 8fb8144020c1350c13fa597bef852f3631b263b6.

--- compiler/GHC/Tc/Deriv/WF.hs
+++ compiler/GHC/Tc/Deriv/WF.hs
@@ -11,6 +11,7 @@
 module GHC.Tc.Deriv.WF ( mk_atat_fam, mk_atat_fam_except
                        , mk_atat_fam_units, mk_atat_fam_except_units
                        , saneTyConForElab
+                       , genWFMirrorTyCons, genWFMirrorTyCon
                        , mkWFCoAxBranch
                        , genWFTyFamInst, genWFTyFamInsts
                        ) where
@@ -252,6 +253,25 @@ getMatchingPredicates' tv tvs preds =
   where qual tv tvs p =
           any (eqType tv) (predTyArgs p)
           && not (or [eqType tv' t | tv' <- tvs, t <- predTyArgs p])
+
+
+genWFMirrorTyCons :: [TyCon] -> TcM [(TyCon, TyCon)]
+genWFMirrorTyCons = mapM genWFMirrorTyCon
+
+genWFMirrorTyCon :: TyCon -> TcM (TyCon, TyCon)
+genWFMirrorTyCon tc
+  | isTypeFamilyTyCon tc && not (isWFMirrorTyCon tc)
+  = do { wf_tc_name <- mk_wf_name $ tyConName tc
+       ; let mirror_tc = mkWFMirrorTyCon
+                         wf_tc_name
+                         constraintKind
+                         tc
+             n_tc      = updateWfMirrorTyCon tc $ Just mirror_tc
+       ; traceTc "wf tf mirror occname:" (ppr wf_tc_name)
+       ; return (mirror_tc, n_tc)
+       }
+  | otherwise
+  = do { pprPanic "wf tf mirror unknown case:" (ppr (famTyConFlav_maybe tc) <+> ppr tc) }
 
 -- given a type family instance equation -
 -- D a b ~ T a b
