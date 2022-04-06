75dd43c084a3e0f1655f2c1a38af5b759e201316
Alex Hubers <ahubers15@cornellcollege.edu>
2022-04-06 12:06:44 -0500

    Show that $wf'NoGhcTc 'Renamed ~ 'Renamed only on import

---
+++ testsuite/tests/party-ctrs/type-families/NoGhcTc.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE AllowAmbiguousTypes     #-}      -- for pprIfTc, etc.
+{-# LANGUAGE ConstraintKinds         #-}
+{-# LANGUAGE DataKinds               #-}
+{-# LANGUAGE DeriveDataTypeable      #-}
+{-# LANGUAGE EmptyDataDeriving       #-}
+{-# LANGUAGE FlexibleContexts        #-}
+{-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE GADTs                   #-}
+{-# LANGUAGE MultiParamTypeClasses   #-}
+{-# LANGUAGE RankNTypes              #-}
+{-# LANGUAGE ScopedTypeVariables     #-}
+{-# LANGUAGE TypeApplications        #-}
+{-# LANGUAGE TypeFamilyDependencies  #-}
+{-# LANGUAGE UndecidableSuperClasses #-} -- for IsPass; see Note [NoGhcTc]
+{-# LANGUAGE UndecidableInstances    #-} -- Wrinkle in Note [Trees That Grow]
+
+module NoGhcTc where
+
+--------------------------------------------------------------------------------
+-- Below is Fluff needed to make the pprXOverLit function fail
+--------------------------------------------------------------------------------
+
+type WFT t = t ~ t
+
+data Pass = Parsed | Renamed | Typechecked
+
+data GhcPass (c :: Pass) where
+  GhcPs :: GhcPass 'Parsed
+  GhcRn :: GhcPass 'Renamed
+  GhcTc :: GhcPass 'Typechecked
+
+type GhcPs = GhcPass 'Parsed
+type GhcRn = GhcPass 'Renamed
+type GhcTc = GhcPass 'Typechecked
+
+type family NoGhcTcPass (p :: Pass) :: Pass where
+  NoGhcTcPass 'Typechecked = 'Renamed
+  NoGhcTcPass other = other
+
+type family XOverli
+    
+foobar :: WFT (GhcPass (NoGhcTcPass p)) => GhcPass p
+foobar = undefined
+
+g' :: GhcPass (NoGhcTcPass 'Renamed)
+g' = GhcRn
---
+++ testsuite/tests/party-ctrs/type-families/NoGhcTcImporter.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE AllowAmbiguousTypes     #-}      -- for pprIfTc, etc.
+{-# LANGUAGE ConstraintKinds         #-}
+{-# LANGUAGE DataKinds               #-}
+{-# LANGUAGE DeriveDataTypeable      #-}
+{-# LANGUAGE EmptyDataDeriving       #-}
+{-# LANGUAGE FlexibleContexts        #-}
+{-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE GADTs                   #-}
+{-# LANGUAGE MultiParamTypeClasses   #-}
+{-# LANGUAGE RankNTypes              #-}
+{-# LANGUAGE ScopedTypeVariables     #-}
+{-# LANGUAGE TypeApplications        #-}
+{-# LANGUAGE TypeFamilyDependencies  #-}
+{-# LANGUAGE UndecidableSuperClasses #-} -- for IsPass; see Note [NoGhcTc]
+{-# LANGUAGE UndecidableInstances    #-} -- Wrinkle in Note [Trees That Grow]
+
+module NoGhcTcImporter where
+
+import NoGhcTc
+
+g' :: GhcPass (NoGhcTcPass 'Renamed)
+g' = GhcRn
